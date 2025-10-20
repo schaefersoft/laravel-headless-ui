21dde21d09e6892349c625e8a4f37e8616a2e373
Luca Schäfer <luca.schaefer1594@gmail.com>
2025-10-20 15:17:21 +0200

    fix(ui): fix data-active duplicate typo

--- resources/views/tabs/tab.blade.php
+++ resources/views/tabs/tab.blade.php
@@ -2,9 +2,8 @@
     type="button"
     data-hui-tab
     @class(['hui-tab', $class])
-    @if($active) data-active data-activa @endif
+    @if($active) data-active @endif
     @if($disabled) data-disabled aria-disabled="true" @endif
 >
     {{$slot}}
 </button>
-
