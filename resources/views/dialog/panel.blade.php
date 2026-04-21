d15d55957850ec6db70af1495e3d27cefd1446d7
Luca Schäfer <luca.schaefer1594@gmail.com>
2026-04-21 12:34:29 +0200

    feat(ui): add maxWith

--- resources/views/dialog/panel.blade.php
+++ resources/views/dialog/panel.blade.php
@@ -1,6 +1,7 @@
 <div
     data-hui-dialog-panel
-    @class(['hui-dialog-panel', $class])
+    @class(['hui-dialog-panel', $maxWidthClass(), $class])
+    {{ $attributes->except(['class', 'maxWidth']) }}
 >
     {{ $slot }}
 </div>
